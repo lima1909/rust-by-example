ed0584d31980a890a1927b7aac7e2fb96934556d
lima1909 <linke.mario@web.de>
2020-04-02 22:11:39 +0200

    trait example implementation

---
+++ trait-examples/src/main.rs
@@ -0,0 +1,45 @@
+trait Draw {
+    fn draw(&self);
+}
+
+#[derive(Debug)]
+struct Screen<'a> {
+    s: &'a str,
+}
+
+impl<'a> Draw for Screen<'a> {
+    fn draw(&self) {
+        println!("draw: {}", self.s);
+    }
+}
+
+#[derive(Debug)]
+struct DrawUser<T>
+where
+    T: Draw,
+{
+    pub draw: T,
+}
+
+impl<T> DrawUser<T>
+where
+    T: Draw,
+{
+    #[allow(dead_code)]
+    fn new(&mut self, d: T) {
+        self.draw = d;
+    }
+}
+
+fn main() {
+    let s = DrawUser {
+        draw: Screen { s: "s" },
+    };
+    println!("{:?}", s);
+
+    let mut sx = DrawUser {
+        draw: Screen { s: "_s" },
+    };
+    sx.new(Screen { s: "sx" });
+    println!("{:?}", sx);
+}
